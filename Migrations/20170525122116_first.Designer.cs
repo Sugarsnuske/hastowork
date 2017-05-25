07fc8101500322ba0053a79063e27cc8a8be9666
Sugarsnuske <rhansen.wi@gmail.com>
2017-05-25 14:22:47 +0200

    SHIT IS WORKING!

---
+++ Migrations/20170525122116_first.Designer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using hastowork.Models;
+
+namespace hastowork.Migrations
+{
+    [DbContext(typeof(MyDbContext))]
+    [Migration("20170525122116_first")]
+    partial class first
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("hastowork.Models.Entities.Movie", b =>
+                {
+                    b.Property<int>("MovieItemID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Director");
+
+                    b.Property<string>("Genre");
+
+                    b.Property<string>("MovieLength");
+
+                    b.Property<string>("MoviePlot");
+
+                    b.Property<string>("MovieTitle");
+
+                    b.Property<int>("Rating");
+
+                    b.Property<int>("YearPublished");
+
+                    b.HasKey("MovieItemID");
+
+                    b.ToTable("Movies");
+                });
+        }
+    }
+}
---
+++ Migrations/MyDbContextModelSnapshot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using hastowork.Models;
+
+namespace hastowork.Migrations
+{
+    [DbContext(typeof(MyDbContext))]
+    partial class MyDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("hastowork.Models.Entities.Movie", b =>
+                {
+                    b.Property<int>("MovieItemID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Director");
+
+                    b.Property<string>("Genre");
+
+                    b.Property<string>("MovieLength");
+
+                    b.Property<string>("MoviePlot");
+
+                    b.Property<string>("MovieTitle");
+
+                    b.Property<int>("Rating");
+
+                    b.Property<int>("YearPublished");
+
+                    b.HasKey("MovieItemID");
+
+                    b.ToTable("Movies");
+                });
+        }
+    }
+}
